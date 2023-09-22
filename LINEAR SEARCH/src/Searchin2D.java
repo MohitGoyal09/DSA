08ba9d4e2d03c8174835e389107e5df3ef6fa35e
MohitGoyal09 <goel0632@gmail.com>
2023-09-22 13:49:07 +0530

    few more stuff

--- LINEAR SEARCH/src/Searchin2D.java
+++ LINEAR SEARCH/src/Searchin2D.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+
 public class Searchin2D {
+    public static void main(String[] args) {
+          int[][] arr = { {3,4, 5, 8},
+                  {56 , 67 , 45 , 78},
+                  {23 , 24 , 235 , 223}
+          };
+          int target = 1;
+          int[] ans = search(arr , target);
+          System.out.println(Arrays.toString(ans));
+    }
+    static int[] search( int[][] arr , int target){
+        for (int row = 0; row < arr.length; row++) {
+            // for each col in every row
+            for (int col = 0; col < arr[row].length; col++) {
+                if ( arr[row][col] == target){
+                    return new int[] { row , col};
+                }
+            }
+
+        }
+        return  new int[]{ -1 ,-1};
+    }
 }
