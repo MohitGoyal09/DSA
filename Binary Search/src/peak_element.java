6fb88a7e3f47a3765a9f4f6692c1301192edeaac
MohitGoyal09 <goel0632@gmail.com>
2024-02-05 14:45:18 +0530

    peak_element

--- Binary Search/src/peak_element.java
+++ Binary Search/src/peak_element.java
@@ -21,7 +21,7 @@ public class peak_element {
         while (low <= high){
             int mid = (low + high) /2;
             if (a[mid] > a[mid-1] && a[mid] > a[mid+1]) return mid;
-            if (a[mid] > a[mid-1]) low = mid + 1;
+            else if (a[mid] > a[mid-1]) low = mid + 1;
             else high = mid - 1;
         }
         return -1;
