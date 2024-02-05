4b55e21a3071d00462b81af0419ce2dae319f75c
MohitGoyal09 <goel0632@gmail.com>
2024-02-05 14:38:20 +0530

    peak_element

---
+++ Binary Search/src/peak_element.java
@@ -0,0 +1,29 @@
+public class peak_element {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,1};
+        System.out.println(bs(a));
+    }
+
+    static int buruutefore(int[] a ){
+        int n = a.length;
+        for (int i = 0 ; i < n ; i++){
+            if ((i == 0 || a[i] > a[i-1]) && ( i == n-1 || a[i] > a[i+1])) return i;
+        }
+        return -1;
+    }
+
+    static int bs(int[] a){
+        int n = a.length;
+        if ( n == 1) return 0;
+        if ( a[0] >a[1]) return 0;
+        if ( a[n-1] > a[n-2]) return n-1;
+        int low = 1 , high = n-2;
+        while (low <= high){
+            int mid = (low + high) /2;
+            if (a[mid] > a[mid-1] && a[mid] > a[mid+1]) return mid;
+            if (a[mid] > a[mid-1]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
