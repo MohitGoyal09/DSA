8012b7d2a13dd2f31be71bbfbf9d29218a812eb6
MohitGoyal09 <goel0632@gmail.com>
2024-02-12 15:35:13 +0530

    typo

--- Binary Search/src/peak_element.java
+++ Binary Search/src/peak_element.java
@@ -15,7 +15,7 @@ public class peak_element {
     static int bs(int[] a){
         int n = a.length;
         if ( n == 1) return 0;
-        if ( a[0] >a[1]) return 0;
+        if ( a[0] > a[1]) return 0;
         if ( a[n-1] > a[n-2]) return n-1;
         int low = 1 , high = n-2;
         while (low <= high){
