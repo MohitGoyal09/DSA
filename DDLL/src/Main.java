69ff6d0665f5c848fddffda970f8349391edce8a
MohitGoyal09 <goel0632@gmail.com>
2024-02-28 15:50:03 +0530

    delete in Double link list

--- DDLL/src/Main.java
+++ DDLL/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         Node head = arrayto2ll(arr);
+        head = kthelement(head, 3); // read
         printLL(head);
     }
     public static Node arrayto2ll(int[] arr){
@@ -20,5 +21,47 @@ public class Main {
             System.out.print(head.data + " ");
             head = head.next;
         }
+    }
+    public static Node deleteHead(Node head){
+        if (head == null || head.next == null) return null;
+        Node prev = head;
+        head = head.next;
+        head.back = null;
+        prev.back = null;
+        return head;
+    }
+    public static Node deleteTail(Node head){
+        if (head == null || head.next == null) return null;
+        Node tail = head;
+        while (tail.next != null) tail = tail.next;
+        Node newtail = tail.back;
+        newtail.next = null;
+        tail.back = null;
+        return head;
+    }
+
+    public static Node kthelement(Node head, int k){
+        if (head == null ) return null;
+        int cnt = 0;
+        Node kthnode = head;
+        while (kthnode != null){
+            cnt++;
+            if (cnt == k) break;
+            kthnode = kthnode.next;
+        }
+        Node prev = kthnode.back;
+        Node front = kthnode.next;
+        if (prev == null && front == null) {
+            return null;
+        } else if (prev == null) {
+            return deleteHead(head);
+        } else if (front == null) {
+            return deleteTail(head);
+        }
+        prev.next = front;
+        front.back = prev;
+        kthnode.next = null;
+        kthnode.back = null;
+        return head;
     }
 }
---
+++ DDLL/src/delete.java
@@ -0,0 +1,11 @@
+public class delete {
+    public static void main(String[] args) {
+
+    }
+    public static void printLL(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+}
