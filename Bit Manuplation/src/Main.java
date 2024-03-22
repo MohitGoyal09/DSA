64f3d3b95ca3b656bdb95fae3607e072022b26e0
MohitGoyal09 <goel0632@gmail.com>
2024-03-22 20:23:18 +0530

    Bit Manuplation

--- Bit Manuplation/src/Main.java
+++ Bit Manuplation/src/Main.java
@@ -1,6 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+    }
+    // ! Flip Bits
+    public static int flipBits(int start , int goal){
+        int ans = start ^ goal;
+        int cnt = 0;
+        for (int i = 0 ; i < 32 ; i++){
+            if ( (ans & (1 << i) ) != 0) cnt++;
+        }
+        return cnt;
+    }
+    // ! Power of 2 
+    public List<List<Integer>> subsets(int[] nums) {
+            int n = nums.length;
+            List<List<Integer>> res = new ArrayList<>();
+            for (int i = 0; i < Math.pow(2, n); i++) {
+                List<Integer> temp = new ArrayList<>();
+
+                for (int j = 0; j < n; j++) {
+                    if ((i & (1 << j)) != 0) {
+                        temp.add(nums[j]);
+                    }
+                }
+
+                res.add(temp);
+            }
+            return res;
+        }
+
+    // ! XOR from L TO R
+    public static int findXOR(int l, int r) {
+        return xor(l - 1) ^ xor(r);
+    }
+
+    public static int xor(int n){
+        if ( n % 4 == 1) return 1;
+        else if ( n % 4 == 2) return n + 1;
+        else if ( n % 4 == 3) return 0;
+        else return n;
     }
 }
--- Bit Manuplation/src/basics.java
+++ Bit Manuplation/src/basics.java
@@ -1,31 +1,50 @@
 public class basics {
-    public static void main(String[] args) {
+    public static void main(String[] args) { }
 
-    }
-
-    static boolean checkKthBit(int n, int k)
-    {
+    static boolean checkKthBit(int n, int k) {
         // * Right shift
         return ((n >> k) & 1) != 0 ;
 
         // * Left Shift
-     //   return ((n << k ) & 1) != 0;
+     // !   return ((n << k ) & 1) != 0;
     }
        // ! Even or Odd
-    public static boolean isEven(int n)
-    {
-        return ((n & 1) == 0);
-
-    }
+    public static boolean isEven(int n) { return ((n & 1) == 0); }
     // ! Set Kth Bit
     static int setKthBit(int N,int K){
-        // code here
         return ((1 << K) | N);
     }
-
-    static int clearKthBit(int N,int K){
-        // code here
+    // ! Clear Kth Bit
+    static int clearKthBit(int N,int K) {
         return (~(1 << K) & N);
     }
+    // ! Toggle kth Bit
+    static int togglekthBit(int N , int K){
+        return (N ^ (1 << K));
+    }
+    // ! Remove the last set Bit
+    static int removeLastSetBit(int N) { return (N & N-1); }
+   // ! Check  number if power of 2
+    static boolean power2(int N){
+        return ( (N & N-1) == 0);
+    }
+    // ! Count Number of set Bits
+    static int countbits(int n){
+        int count = 0;
+        while ( n > 1){
+            count += n & 1;
+            n = n >> 1;
+        }
+        if (n == 1) count++;
+        return count;
+    }
 
+    static int countbits2(int n){
+        int count = 0;
+        while (n != 0){
+            n = n & n-1;
+            count++;
+        }
+        return count;
+    }
 }
