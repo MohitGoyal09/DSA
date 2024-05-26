426330a83abfadc8f651cc0fa8d4c1ddbd33b75b
MohitGoyal09 <goel0632@gmail.com>
2024-05-26 20:02:29 +0530

    Combination_Sum

--- Recursion/patternwise/src/Combination_Sum.java
+++ Recursion/patternwise/src/Combination_Sum.java
@@ -50,3 +50,29 @@ public class Combination_Sum {
 
     }
 }
+// ! Combination 3
+class Solution3 {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> path = new ArrayList<>();
+        helper(nums, n, 0, path, res, k);
+        return res;
+    }
+    private void helper(int[] nums, int target, int startIndex, List<Integer> path, List<List<Integer>> res, int k) {
+        if (path.size() == k) {
+            if (target == 0) {
+                res.add(new ArrayList<Integer>(path));
+            }
+            return;
+        }
+        for (int i = startIndex; i < nums.length; i++) {
+            if (nums[i] > target) {
+                break;
+            }
+            path.add(nums[i]);
+            helper(nums, target - nums[i], i + 1, path, res, k);
+            path.remove(path.size() - 1);
+        }
+    }
+}
