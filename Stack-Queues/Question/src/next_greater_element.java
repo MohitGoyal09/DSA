ea396740a6742760d19d0b26fbdd8f828e4608a3
MohitGoyal09 <goel0632@gmail.com>
2024-06-02 17:24:20 +0530

    nge

--- Stack-Queues/Question/src/next_greater_element.java
+++ Stack-Queues/Question/src/next_greater_element.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Stack;
+
 // ! https://leetcode.com/problems/next-greater-element-i/solutions/5164552/must-read-intuitive-pattern-for-deep-understanding-of-nge-and-nsm
 public class next_greater_element {
     public static void main(String[] args) {
@@ -34,6 +35,25 @@ class Solution {
         Stack<Integer> s1=new Stack<>();
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=n2.length-1;i>=0;i--){
+            int num=n2[i];
+            while(!s1.isEmpty() && s1.peek()<=num){
+                s1.pop();
+            }
+            if(s1.isEmpty()) map.put(num,-1);
+            else map.put(num,s1.peek());
+
+            s1.push(num);
+        }
+
+        for (int i = 0; i < n1.length; i++) {
+            n1[i] = map.getOrDefault(n1[i], -1);
+        }
+        return n1;
+    }
+    public int[] nextsmallestElement(int[] n1, int[] n2) {
+        Stack<Integer> s1=new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i< n2.length;i++){
             int num=n2[i];
             while(!s1.isEmpty() && s1.peek()<=num){
                 s1.pop();
