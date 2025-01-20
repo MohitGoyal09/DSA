011284451d7e8fe862fd710b196ad26cb966eb47
MohitGoyal09 <goel0632@gmail.com>
2025-01-20 18:57:49 +0530

    POTD

---
+++ Arrays/Medium/src/first_completed_painted_row_col.java
@@ -0,0 +1,124 @@
+import java.util.*;
+
+public class first_completed_painted_row_col {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            mp.put(arr[i], i);
+        }
+
+        int minIndex = Integer.MAX_VALUE;
+        for (int[] value : mat) {
+            int lastIndex = Integer.MIN_VALUE;
+            for (int col = 0; col < n; col++) {
+                int val = value[col];
+                int idx = mp.get(val);
+                lastIndex = Math.max(lastIndex, idx);
+            }
+
+            minIndex = Math.min(minIndex, lastIndex);
+        }
+        for (int col = 0; col < n; col++) {
+            int lastIndex = Integer.MIN_VALUE;
+
+            for (int[] ints : mat) {
+                int val = ints[col];
+                int idx = mp.get(val);
+                lastIndex = Math.max(lastIndex, idx);
+            }
+
+            minIndex = Math.min(minIndex, lastIndex);
+        }
+
+        return minIndex;
+    }
+
+    // Approach - 2 (Better Approach)
+// T.C : O(m*n)
+// S.C : O((m*n) + m + n)
+    class SolutionBetter {
+
+        public int firstCompleteIndex(int[] arr, int[][] mat) {
+            int m = mat.length;
+            int n = mat[0].length;
+
+            Map<Integer, int[]> mp = new HashMap<>(); // stores value to cell-coordinate [i][j]
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    int val = mat[i][j];
+                    mp.put(val, new int[]{i, j});
+                }
+            }
+
+            int[] rowCountPaint = new int[m]; // [i] = how many painted in row i
+            int[] colCountPaint = new int[n]; // [i] = how many painted in col i
+
+            for (int i = 0; i < arr.length; i++) {
+                int val = arr[i];
+                int[] coordinates = mp.get(val);
+                int row = coordinates[0], col = coordinates[1];
+
+                rowCountPaint[row]++;
+                colCountPaint[col]++;
+
+                if (rowCountPaint[row] == n || colCountPaint[col] == m) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+    // Approach - 1 (Brute Force)
+// T.C : O((m*n) * s*(m+n)) , where s = arr.size() which is (m*n)
+// S.C : O(m*n)
+    class Solution {
+
+        private boolean checkRowPainted(int[][] mat, int row) {
+            for (int col = 0; col < mat[0].length; col++) {
+                if (mat[row][col] > 0) { // not painted
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private boolean checkColPainted(int[][] mat, int col) {
+            for (int row = 0; row < mat.length; row++) {
+                if (mat[row][col] > 0) { // not painted
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int firstCompleteIndex(int[] arr, int[][] mat) {
+            int m = mat.length;
+            int n = mat[0].length;
+
+            Map<Integer, int[]> mp = new HashMap<>(); // stores value to cell-coordinate [i][j]
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    int val = mat[i][j];
+                    mp.put(val, new int[]{i, j});
+                }
+            }
+
+            for (int i = 0; i < arr.length; i++) {
+                int val = arr[i];
+                int[] coordinates = mp.get(val);
+                int row = coordinates[0], col = coordinates[1];
+                mat[row][col] *= -1; // painted
+
+                if (checkRowPainted(mat, row) || checkColPainted(mat, col)) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
