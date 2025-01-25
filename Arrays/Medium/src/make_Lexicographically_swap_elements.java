9e2193637df8a0fd63d876767252cd133b2e1907
MohitGoyal09 <goel0632@gmail.com>
2025-01-25 11:09:45 +0530

    POTD

---
+++ Arrays/Medium/src/make_Lexicographically_swap_elements.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class make_Lexicographically_swap_elements {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+        int[] vec = nums.clone();
+        Arrays.sort(vec);
+
+        int groupNum = 0;
+        Map<Integer, Integer> numToGroup = new HashMap<>();
+        Map<Integer, LinkedList<Integer>> groupToList = new HashMap<>();
+
+        numToGroup.put(vec[0], groupNum);
+        groupToList.putIfAbsent(groupNum, new LinkedList<>());
+        groupToList.get(groupNum).add(vec[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (Math.abs(vec[i] - vec[i - 1]) > limit) {
+                groupNum++;
+            }
+            numToGroup.put(vec[i], groupNum);
+            groupToList.putIfAbsent(groupNum, new LinkedList<>());
+            groupToList.get(groupNum).add(vec[i]);
+        }
+
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int group = numToGroup.get(num);
+            result[i] = groupToList.get(group).pollFirst(); // Use and remove the smallest element
+        }
+
+        return result;
+    }
+}
